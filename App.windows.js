ea814158188ff4e87ca912f6a3f445590fb1e571
bpsourav21 <bpsourav21@gmail.com>
2018-02-22 15:27:11 +0600

    first commit

---
+++ App.windows.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import App from './App.js';
+
+export default class WinApp extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
